81a8ed9fb5a49effc85675191e16950ba7ff2d9b
Gabatawr <gabatawr@gmail.com>
2021-06-11 20:39:13 +0300

    dev

--- src/js/main.js
+++ src/js/main.js
@@ -49,7 +49,7 @@ class News {
   // #region elements
 
   _elRefresh(elName) {
-    for (let i = 0; i < this.node.childNodes.length; i++) {
+    for (let i = 0; i < this.node.childNodes.length; i += 1) {
       if (
         this.node.childNodes[i].nodeName !== '#text'
         && this.node.childNodes[i].className === this.pref + elName
@@ -78,21 +78,19 @@ class News {
   // #region date
 
   get date() {
-    let daysAgo = Math.floor(
-      (new Date().getTime() - this._date.getTime()) / (1000 * 3600 * 24)
-    );
+    const daysAgo = Math.floor((new Date().getTime() - this._date.getTime()) / (1000 * 3600 * 24));
     if (daysAgo < 8) {
-      if (daysAgo == 0) return 'Today';
-      return daysAgo + ' days ago';
+      if (daysAgo === 0) return 'Today';
+      return `${daysAgo} days ago`;
     }
 
-    let ret = [
+    const ret = [
       this._date.getDate(),
       this._date.getMonth() + 1,
       this._date.getYear() + 1900,
     ];
-    for (let i = 0; i < 2; i++) {
-      ret[i] = ret[i] > 9 ? ret[i] : '0' + `${ret[i]}`;
+    for (let i = 0; i < 2; i += 1) {
+      ret[i] = ret[i] > 9 ? ret[i] : `0${ret[i]}`;
     }
     return ret.join('.');
   }
@@ -111,6 +109,7 @@ class News {
   get content() {
     return this._content;
   }
+
   set content(value) {
     if (value !== undefined) {
       this._content = value;
@@ -123,22 +122,21 @@ class News {
   // #region tagList
 
   _tagItemInit(ul, tag) {
-    let li = document.createElement('li');
-    li.classList.add(this.pref + 'tag-item');
+    const li = document.createElement('li');
+    li.classList.add(`${this.pref}tag-item`);
     li.textContent = tag;
 
     ul.appendChild(li);
   }
 
   _tagListInit() {
-    let ul = this.template.content
-      .querySelector('.' + this.pref + 'tag-list')
+    const ul = this.template.content
+      .querySelector(`.${this.pref}tag-list`)
       .cloneNode(true);
 
-    ul.querySelector('.' + this.pref + 'tag-item').textContent =
-      this._tagList[0];
+    [ul.querySelector(`.${this.pref}tag-item`).textContent] = this._tagList;
 
-    for (let i = 1; i < this._tagList.length; i++) {
+    for (let i = 1; i < this._tagList.length; i += 1) {
       this._tagItemInit(ul, this._tagList[i]);
     }
     this.node.appendChild(ul);
@@ -147,6 +145,7 @@ class News {
   get tagList() {
     return this._tagList;
   }
+
   set tagList(value) {
     if (value !== undefined) {
       this._tagList = value;
@@ -157,14 +156,11 @@ class News {
   }
 
   addTag(tag) {
-    if (this._tagList.length == 0) {
+    if (this._tagList.length === 0) {
       this.tagList = this._tagList.concat(tag);
-    } else if (this._tagList.includes(tag) == false) {
+    } else if (this._tagList.includes(tag) === false) {
       this._tagList.push(tag);
-      this._tagItemInit(
-        this.node.querySelector('.' + this.pref + 'tag-list'),
-        tag
-      );
+      this._tagItemInit(this.node.querySelector(`.${this.pref}tag-list`), tag);
     }
   }
 
@@ -174,30 +170,28 @@ class News {
 }
 
 const dayDecrement = (date) => {
-  let d = new Date(date);
+  const d = new Date(date);
 
   if (d.getDate() - 1 === 0) {
     if (d.getMonth() - 1 === 0) {
       d.setYear(d.getYear() - 1);
       d.setMonth(12);
       d.setDate(31);
-    }
-    else {
+    } else {
       d.setMonth(d.getMonth() - 1);
       d.setDate(new Date(d.getYear(), d.getMonth(), 0).getDate());
     }
-  }
-  else {
+  } else {
     d.setDate(d.getDate() - 1);
   }
 
   return new Date(d);
-}
+};
 
 const newsBlock = document.querySelector('.news');
 const newsTemplate = newsBlock.querySelector('.news__item-template');
 
-let newsList = [
+const newsList = [
   new News({
     template: newsTemplate,
     title: 'What is Lorem Ipsum?',
@@ -205,11 +199,11 @@ let newsList = [
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     tags: ['lorem', 'ipsum', 'text'],
-  })
+  }),
 ];
 
 const NEWS_SIZE = Math.floor(Math.random() * 31);
-for (let i = 0, date = new Date(); i < NEWS_SIZE; i++) {
+for (let i = 0, date = new Date(); i < NEWS_SIZE; i += 1) {
   date = dayDecrement(date);
 
   newsList.push(
@@ -219,7 +213,7 @@ for (let i = 0, date = new Date(); i < NEWS_SIZE; i++) {
       date: date.toDateString(),
       content: lorem.generateParagraphs(1),
       tags: lorem.generateWords(Math.floor(Math.random() * 5)).split(' '),
-    })
+    }),
   );
 }
 
